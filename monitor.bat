7728b42d00634632e1394c1646632f099f7d625f
Assil Abdulrahim <assil@365Architect.com>
2026-05-09 14:00:10 -0400

    Useful dash Signed-off-by: Assil Abdulrahim <assil@365Architect.com>

--- monitor.bat
+++ monitor.bat
@@ -1,4 +1,8 @@
 @echo off
 set HOST=assilabdulrahim@192.168.86.24
 
-wt -w new new-tab --title "dashboard" cmd /k ssh %HOST% ; split-pane -H --size 0.5 cmd /k ssh %HOST% ; split-pane -V --size 0.55 cmd /k "ssh -tt %HOST% nvtop" ; split-pane -V --size 0.5 cmd /k "ssh %HOST% tail -f /var/log/syslog" ; split-pane -V --size 0.5 cmd /k "ssh -tt %HOST% /home/assilabdulrahim/watch-mem.sh" ; split-pane -V --size 0.6 cmd /k "ssh -tt %HOST% htop" ; split-pane -V --size 0.5 cmd /k "ssh -tt %HOST% iostat -xz 1"
+wt -w new new-tab --title "dashboard" cmd /k ssh %HOST% ; ^
+split-pane -H --size 0.5 cmd /k ssh %HOST% ; ^
+split-pane -V --size 0.65 cmd /k "ssh -tt %HOST% nvtop" ; ^
+split-pane -V --size 0.5 cmd /k "ssh -tt %HOST% /home/assilabdulrahim/watch-mem.sh" ; ^
+split-pane -V --size 0.5 cmd /k "ssh %HOST% journalctl -p 3 -f"

0bcd6df4b25616263f05592bd551248599d2ade1
Assil Abdulrahim <assil@365Architect.com>
2026-05-09 13:45:15 -0400

    Monitoring features Signed-off-by: Assil Abdulrahim <assil@365Architect.com>

--- monitor.bat
+++ monitor.bat
@@ -1,13 +1,10 @@
 @echo off
 set HOST=assilabdulrahim@192.168.86.24
 
-wt -w new ^
-new-tab --title "dashboard" cmd /k ssh %HOST% ^
-; split-pane -H --size 0.6 cmd /k ssh %HOST% ^
-; move-focus left ^
-; split-pane -V --size 0.6 cmd /k ssh %HOST% ^
-; move-focus right ^
-; split-pane -V --size 0.6 cmd /k ssh %HOST% ^
-; move-focus down ^
-; split-pane -H --size 0.7 cmd /k "ssh -tt %HOST% /home/assilabdulrahim/watch-mem.sh" ^
-; split-pane -H --size 0.3 cmd /k "ssh -tt %HOST% nvtop"
+wt -w new new-tab --title "dashboard" cmd /k ssh %HOST% ; ^
+split-pane -H --size 0.5 cmd /k ssh %HOST% ; ^
+move-focus left ; split-pane -V --size 0.5 cmd /k "ssh -tt %HOST% htop" ; ^
+move-focus right ; split-pane -V --size 0.5 cmd /k "ssh %HOST% tail -f /var/log/syslog" ; ^
+move-focus down ; split-pane -V --size 0.60 cmd /k "ssh -tt %HOST% nvtop" ; ^
+split-pane -H --size 0.65 cmd /k "ssh -tt %HOST% /home/assilabdulrahim/watch-mem.sh" ; ^
+split-pane -H --size 0.35 cmd /k "ssh -tt %HOST% iostat -xz 1"

a0b50a8a1394b570bb1e2c8c6de198283f31d12b
Assil Abdulrahim <assil@365Architect.com>
2026-05-09 13:38:11 -0400

    Signed-off-by: Assil Abdulrahim <assil@365Architect.com>

--- monitor.bat
+++ monitor.bat
@@ -1,17 +1,13 @@
 @echo off
+set HOST=assilabdulrahim@192.168.86.24
 
 wt -w new ^
-new-tab -t "dashboard" ^
-cmd /k "ssh -t assilabdulrahim@192.168.86.24 nvtop" ^
-; split-pane -H --size 0.55 ^
-cmd /k "ssh -t assilabdulrahim@192.168.86.24 /home/assilabdulrahim/watch-mem.sh" ^
-; split-pane -V --size 0.5 ^
-cmd /k "ssh assilabdulrahim@192.168.86.24" ^
-; move-focus down ^
-; split-pane -H --size 0.5 ^
-cmd /k "ssh assilabdulrahim@192.168.86.24" ^
-; split-pane -V --size 0.5 ^
-cmd /k "ssh assilabdulrahim@192.168.86.24" ^
+new-tab --title "dashboard" cmd /k ssh %HOST% ^
+; split-pane -H --size 0.6 cmd /k ssh %HOST% ^
+; move-focus left ^
+; split-pane -V --size 0.6 cmd /k ssh %HOST% ^
 ; move-focus right ^
-; split-pane -V --size 0.5 ^
-cmd /k "ssh assilabdulrahim@192.168.86.24"
+; split-pane -V --size 0.6 cmd /k ssh %HOST% ^
+; move-focus down ^
+; split-pane -H --size 0.7 cmd /k "ssh -tt %HOST% /home/assilabdulrahim/watch-mem.sh" ^
+; split-pane -H --size 0.3 cmd /k "ssh -tt %HOST% nvtop"
